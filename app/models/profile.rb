ed863513e498d46e6c65a4222f368d02bd2aa8f8
Lorin Thwaits <lorint@gmail.com>
2016-12-14 18:24:11 +0000

    Styling and such for avatars and default avatars

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,7 +1,17 @@
 class Profile < ApplicationRecord
   belongs_to :customer
-  belongs_to :default_avatar, required: false
+  belongs_to :default_avatar, class_name: "Avatar", required: false
 
   has_many :avatars
   accepts_nested_attributes_for :avatars
+
+  before_save :set_default_avatar
+
+  private
+
+  def set_default_avatar
+    if self.avatars.count == 1
+      self.default_avatar = self.avatars.first
+    end
+  end
 end
